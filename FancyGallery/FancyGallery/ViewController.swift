f63c57b4b9952415c2795321205c8011a4b9b4db
Mauricio Esteves <mesteves@kineticcafe.com>
2019-12-09 20:06:46 -0500

    Starting the project from scratch and adding first features

---
+++ FancyGallery/FancyGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FancyGallery
+//
+//  Created by Mauricio Esteves on 2019-12-09.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
